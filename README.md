295efda21ce17f480df36ce63ed10fbf95c905c8
Cristian Cardozo <cristian.cardozo@ticketmaster.com>
2022-12-28 08:57:41 -0500

    DDBB connection config.

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ This is a project that I create following the tutorial on youtube [Curso Spring]
 In this project I implement the spring security the basic login. Here I use Maven to handle dependencies.
 Also create a custom login using boostrap framework, implement logout and identify the roles. 
 Limit access to certain pages based on the role. Create custom error pages. Hidden resources based on roles.
+Set dependencies to connect with database (MySQL) and handle the pool connections, create properties file for jdbc driver 
+and connection pool, and specify it in the configuration of Spring.
 
 [def]: https://www.youtube.com/playlist?list=PLU8oAlHdN5Blq85GIxtKjIXdfHPksV_Hm
--- src/main/java/co/camcar/seguridad/spring/config/App.java
+++ src/main/java/co/camcar/seguridad/spring/config/App.java
@@ -1,11 +1,21 @@
 package co.camcar.seguridad.spring.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 /**
  * Hello world!
@@ -14,9 +24,15 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "co.camcar.seguridad.spring")
+@PropertySource("classpath:persistencia-mysql.properties")
 public class App 
 {
 	
+	@Autowired
+	private Environment env;
+	
+	//Sistema de log
+	private Logger log = Logger.getLogger(getClass().getName());
 	
 	@Bean
 	public ViewResolver viewResolver() {
@@ -24,5 +40,42 @@ public class App
 		resolver.setPrefix("/WEB-INF/view/");
 		resolver.setSuffix(".jsp");
 		return resolver;
+	}
+	
+	//bean para seguridad
+	@Bean
+	public DataSource seguridadDataSource() {
+		//crear pool de conexiones
+		
+		ComboPooledDataSource poolDataSource = new ComboPooledDataSource();
+		
+		//Establecer driver JDBC
+		
+		try {
+			poolDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			log.info("ERROR - "+e.getMessage());
+			e.printStackTrace();
+		}
+		
+		//Hacer log de las propiedades de conexión
+		
+		log.info("URL de la BBDD: " + env.getProperty("jdbc.url"));
+		log.info("usuario de la BBDD: " + env.getProperty("jdbc.user"));
+		
+		//Establecer propiedades de conexión con BBDD
+		
+		poolDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		poolDataSource.setUser(env.getProperty("jdbc.user"));
+		poolDataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		//Establecer las propiedades del pool de conexiones
+		
+		poolDataSource.setInitialPoolSize(Integer.valueOf(env.getProperty("connection.pool.initialPoolSize")));
+		poolDataSource.setMinPoolSize(Integer.valueOf(env.getProperty("connection.pool.minPoolSize")));
+		poolDataSource.setMaxPoolSize(Integer.valueOf(env.getProperty("connection.pool.maxPoolSize")));
+		poolDataSource.setMaxIdleTime(Integer.valueOf(env.getProperty("connection.pool.maxIdleTime")));
+		
+		return poolDataSource;
 	}
 }
